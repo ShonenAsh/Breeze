7b3373f68e3f9f9ed17fe04a444df13db3b81e95
Ashish Magadum <shonenash@gmail.com>
2020-10-08 20:02:49 +0530

    Location search Autocomplete

--- app/src/main/java/com/shonenash/breeze/MainActivity.kt
+++ app/src/main/java/com/shonenash/breeze/MainActivity.kt
@@ -1,13 +1,25 @@
 package com.shonenash.breeze
 
 import android.os.Bundle
-import com.google.android.material.floatingactionbutton.FloatingActionButton
-import com.google.android.material.snackbar.Snackbar
-import androidx.appcompat.app.AppCompatActivity
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.gms.common.api.Status
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.libraries.places.api.Places
+import com.google.android.libraries.places.api.model.Place
+import com.google.android.libraries.places.api.model.RectangularBounds
+import com.google.android.libraries.places.api.model.TypeFilter
+import com.google.android.libraries.places.api.net.PlacesClient
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+    var placesClient: PlacesClient? = null
+    private val TAG = "MainActivity"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -16,8 +28,36 @@ class MainActivity : AppCompatActivity() {
 
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                    .setAction("Action", null).show()
+                .setAction("Action", null).show()
         }
+
+        if (!Places.isInitialized()) {
+            Places.initialize(applicationContext, resources.getString(R.string.api_key))
+        }
+        placesClient = Places.createClient(this)
+
+        val autocompleteFragment =
+            supportFragmentManager.findFragmentById(R.id.autocomplete_fragment) as AutocompleteSupportFragment
+
+        autocompleteFragment.setTypeFilter(TypeFilter.CITIES)
+        autocompleteFragment.setLocationBias(
+            RectangularBounds.newInstance(
+                LatLng(-33.880490, 151.184363),
+                LatLng(-33.858754, 151.229596)
+            )
+        )
+
+        autocompleteFragment.setCountries("IN")
+        autocompleteFragment.setPlaceFields(listOf(Place.Field.ID, Place.Field.NAME))
+        autocompleteFragment.setOnPlaceSelectedListener(object : PlaceSelectionListener {
+            override fun onPlaceSelected(place: Place) {
+                Log.i(TAG, "Place: " + place.getName() + ", " + place.getId());
+            }
+
+            override fun onError(status: Status) {
+                Log.i(TAG, "An error occurred: " + status);
+            }
+        })
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
